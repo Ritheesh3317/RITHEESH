736b565a944a5a4409a8cbcf57b3719b10f4ddf7
Ritheesh3317 <113654592+Ritheesh3317@users.noreply.github.com>
2022-09-16 08:55:34 +0530

    sample.java

---
+++ sample.java
@@ -0,0 +1,16 @@
+package forloop;
+
+public class forloopexample {
+    public static void main(String[] args) {
+        int num=0;
+        for (int i=1; i<=5;i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print(num+1);
+                num=num+1;
+            }
+            System.out.println();
+        }
+    }
+}
